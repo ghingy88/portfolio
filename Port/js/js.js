a8b595071b3657d1f164603787eb9810fb2e2fb0
Ghingmo Luk <89243160+ghingy88@users.noreply.github.com>
2022-02-22 19:10:35 +1100

    Add files via upload

---
+++ Port/js/js.js
@@ -0,0 +1,19 @@
+//button clicker//
+var clicks = 0; // change int to var here
+    function countClicks() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+
+//COMMON NAV AND FOOTER//
+    $(function(){
+        $("#nav-placeholder").load("assets/php/nav.html");
+      });
+
+      $(function(){
+        $("#footer-placeholder").load("assets/php/footer.html");
+      });
+//END COMMON NAV AND FOOTER//\
+
+
+//PRELOADER//
