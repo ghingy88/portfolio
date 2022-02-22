addcb23ce3d4f87d9ba668119e3f6e4266cd4f24
Ghingmo Luk <89243160+ghingy88@users.noreply.github.com>
2022-02-22 14:59:19 +1100

    Delete js directory

--- js/js.js
+++
@@ -1,6 +0,0 @@
-//button clicker//
-var clicks = 0; // change int to var here
-    function countClicks() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    };
